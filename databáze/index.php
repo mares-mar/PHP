e99a8242b50062e32c3bf5d146ed72ece2f1ea27
Jmeno <youremail@example.com>
2026-02-24 09:39:07 +0100

    začíná to fungovat, dodělej databáze gui

---
+++ databáze/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include "database.php"?>
+
+    <form action="">
+
+    <h1>Přidej do databaze</h1>
+
+    <br>
+    <h1>Smaž id z databáze</h1>
+    <br>
+    </form>
+    
+</body>
+</html>
